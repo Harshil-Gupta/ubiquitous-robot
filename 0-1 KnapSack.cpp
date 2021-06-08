266ff4b45a94f1aa70b597dc85999a84ac86dba3
Harshil Gupta <53968071+Harshil-Gupta@users.noreply.github.com>
2021-06-08 06:00:29 +0530

    Updated 0-1 Knapsack
    Updated Wrong Code of DP.

--- 0-1 KnapSack.cpp
+++ 0-1 KnapSack.cpp
@@ -1,23 +1,19 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-int knapsack_dp(int *weights, int *values, int n, int maxWeight){
-
-    int dp[n+1][maxWeight+1];
-    for(int i=0;i<=n;i++){
-        dp[i][0] = 0;
-    }
-    for(int j=0;j<=maxWeight;j++){
-        dp[0][j] = 0;
-    }
-
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=maxWeight;j++){
-            if(weights[i-1] <= maxWeight){
-                dp[i][j] = max(dp[i-1][j],values[i-1] + dp[i-1][maxWeight-weights[i-1]];
+int knapsack_dp(int *weights, int *values, int n, int maxWeight)
+{
+    int dp[n + 1][maxWeight + 1];
+    for (int i = 0; i <= n; i++){
+        for (int j = 0; j <= maxWeight; j++){
+            if (i == 0 || j == 0){
+                dp[i][j] = 0;
             }
-            else{        
-                dp[i][j] = dp[(i-1)][j];
+            else if (weights[i - 1] <= j){
+                dp[i][j] = max(dp[i - 1][j], values[i - 1] + dp[i - 1][j - weights[i - 1]]);
+            }
+            else{
+                dp[i][j] = dp[i - 1][j];
             }
         }
     }
